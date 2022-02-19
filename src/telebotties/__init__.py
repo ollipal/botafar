17757479b6e05a9fa2c03888f5a6eb92862b4208
Olli Paloviita <olli.paloviita@gmail.com>
2022-02-19 15:20:07 +0200

    tb.version added

--- src/telebotties/__init__.py
+++ src/telebotties/__init__.py
@@ -2,6 +2,9 @@
 
 __version__ = "0.0.1"
 
+
 from ._internal.cli import _cli  # This enables `telebotties` from cli
 from ._internal.inputs import Button
 from ._internal.main import listen
+
+version = __version__
