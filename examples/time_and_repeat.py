3022d8a0f9b497682c2dd850ddb0bb85a82a3c89
Olli Paloviita <olli.paloviita@gmail.com>
2022-10-04 19:15:10 +0300

    examples: on_repeat, on_time and time() example added

---
+++ examples/time_and_repeat.py
@@ -0,0 +1,35 @@
+import botafar
+
+"""
+A simple example how to use on_time, on_repeat and botrafar.time()
+"""
+
+
+# Repeats callback with one second in between calls
+# botafar.time() is used here to get the current time
+@botafar.on_repeat(sleep=1)
+def once_second():
+    botafar.print(f"repeating {botafar.time()}")
+
+
+# Triggers after 5 seconds, if player has not disconnected before
+@botafar.on_time(3)
+def after_3_seconds():
+    botafar.print("3 seconds passed")
+
+
+# Triggers after 10 and 12 seconds, if player has not disconnected before
+# Note the optional 'time' parameter that gets filled automatically
+@botafar.on_time(5, 8)
+def after_5_and_8_seconds(time):
+    botafar.print(f"{time} seconds passed")
+
+
+# Countdown demo by unpacking a range
+@botafar.on_time(*range(10, 15))
+def countdown(time):
+    botafar.print(f"Time left {15 - time}")
+
+
+b = botafar.Button("SPACE")  # This enables remote controlling
+botafar.run()
